c16644b347fc6f9930e83b6ee0864cdfac955088
CAVE-JP\JP <jiri.polcar@seznam.cz>
2018-05-31 15:06:56 +0200

    css a kostra
    vylepšní kostry

---
+++ scripts/include.js
@@ -0,0 +1,18 @@
+function includeMenu() {
+    document.write("<div class=\"navbar\">");
+    document.write("<a href=\"#\">About me</a><a href=\"#\">My projects</a><a href=\"#\">Blog</a><a href=\"#\">Contact</a>");
+    document.write("</div>");
+}
+
+function includeHeader() {
+    document.write("<div class=\"header\">");
+    document.write("<h1>title</h1><h2>subtitle</h2>");
+    document.write("</div>");
+}
+
+function includeFooter() {
+    document.write("<div class=\"footer\">");
+    document.write("<h1>footer</h1><h2>fodder</h2>");
+    document.write("</div>");
+}
+
